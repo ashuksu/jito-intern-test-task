2a4582af9605f824f0e362e90923b8c2ec64c417
Denys Liutyi <ashuksu@gmail.com>
2026-09-23 23:30:33 +0200

    feature/TASK-1 - Add parseHtml function to convert HTML string to AST JSON object

--- parser/index.js
+++ parser/index.js
@@ -1,7 +1,15 @@
 import {tokenize} from "./tokenizer.js";
 import {buildTree} from "./tree-builder.js";
 
+/**
+ * Converts HTML string to AST JSON object.
+ * @param {string} htmlText
+ * @returns {Array<Object>}
+ */
 export function parseHtml(htmlText) {
+    if (typeof htmlText !== "string") {
+        return [];
+    }
     const tokens = tokenize(htmlText);
 
     return buildTree(tokens);
